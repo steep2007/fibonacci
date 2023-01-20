c27fba71aeed33a775c51b8564908bd5f9ae1254
stephani <steehin01@gmail.com>
2023-01-20 16:15:26 -0400

    new fibonacci code

--- fibonacci real.py
+++
@@ -1,9 +0,0 @@
-seq = int(input("Enter your sequence number: "))
-num1 = 0
-num2 = 1
-print(f'{num1} - {num2}', end=' ')
-for i in range(0,seq -2):
-    ressom = num1 + num2
-    num1 = num2
-    num2 = ressom
-    print(f'- {ressom}', end=' ')
--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,18 @@
-num = int(input("Enter your number: "))
-numOuro = input("Do you want to find out the golden number of your number? Y(yes) ou N(no)")
-if num == 0:
-    print('0')
-if num == 1 or num == 2:
-    print('1')
-else:
-    s1 = num - 1
-    s2 = num - 2
-    som = s1 + s2
-    print(som)
+seq = int(input("Enter your sequence number: "))
+numOuro = input("Do you want to see the golden number of the last digit shown in your sequence? Y(yes) or N(no)")
+num1 = 0
+num2 = 1
+print(f'{num1} - {num2}', end=' ')
+for i in range(0,seq -2):
+    ressom = num1 + num2
+    num1 = num2
+    num2 = ressom
+    print(f'- {ressom}', end=' ')
+
 if numOuro == 'N':
     ('Byebye 😊')
     exit(0)
 if numOuro == 'Y':
-    sO = num - 1
-    div = num / sO
+    sO = ressom - 1
+    div = ressom / sO
     print('Your golden number is {}' .format(div))
